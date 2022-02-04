aead8e200d215674d6176648614fe03649e61365
Akash B G <akashbpura@gmail.com>
2022-02-04 13:28:20 +0530

    Modified to calculate Employee Daily wage and store it

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -14,14 +14,22 @@ switch (empCheck) {
         return 0;
     }
 }
-const MAX_HRS_IN_MONTH = 100;
+function calcDailyWage(empHrs) {
+    return empHrs*WAGE_PER_HOUR;
+}
+const MAX_HRS_IN_MONTH = 160;
 const NUM_OF_WORKING_DAYS = 20;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
 let empCheck = Math.floor(Math.random()*10)%3;
-totalEmpHrs = getWorkingHours(empCheck);
+let empHrs = getWorkingHours(empCheck);
+totalEmpHrs += empHrs;
+empDailyWageArr.push(calcDailyWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+
+let empWage = calcDailyWage(totalEmpHrs);
 console.log("Total Employee worked days: "+totalWorkingDays+" and total worked hours: "+totalEmpHrs+" and the Wage is: "+empWage);
