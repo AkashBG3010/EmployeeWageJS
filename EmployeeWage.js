f74b713f902a4b7d4a532aab7db783d5ac0c84d3
Akash B G <akashbpura@gmail.com>
2022-02-04 15:07:23 +0530

    Modified to story wage with map function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -24,6 +24,7 @@ function calcDailyWage(empHrs) {
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArr = new Array();
+let empDailyWageMap = new Map();
 
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
@@ -31,6 +32,7 @@ let empCheck = Math.floor(Math.random()*10)%3;
 let empHrs = getWorkingHours(empCheck);
 totalEmpHrs += empHrs;
 empDailyWageArr.push(calcDailyWage(empHrs));
+empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
 }
 
 let empWage = calcDailyWage(totalEmpHrs);
@@ -80,3 +82,9 @@ function totalDaysWorked(numOfDays, dailyWage) {
     if (dailyWage>0) return numOfDays+1;
     return numOfDays;
 }
+
+console.log(empDailyWageMap);
+function totalWages(totalWage, dailyWage) {
+    return totalWage + dailyWage;
+}
+console.log("Employee Wage Map Total Hours: "+Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
